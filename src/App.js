1af3073697885d62e48a9be6850a2a266f85b89b
caminobelllo <alstpgml0812@gmail.com>
2024-07-02 16:07:57 +0900

    feat: route 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Items from "./pages/Items";
+import Members from "./pages/Members";
+import Orders from "./pages/Orders";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/items" element={<Items />} />
+      <Route path="/members" element={<Members />} />
+      <Route path="/orders" element={<Orders />} />
+    </Routes>
+  );
 }
 
 export default App;

2846bfff31f523973ea598b4a2fc95c9ab501de7
caminobelllo <alstpgml0812@gmail.com>
2024-07-03 15:02:49 +0900

    chore: react-table 패키지 설치

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -1,17 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { axiosInstance } from "../api";
+import ItemList from "../components/items/ItemList";
+import styled from "styled-components";
 
 const Items = () => {
+  const [items, setItems] = useState([]);
+
   const getItems = async () => {
     const response = await axiosInstance.get("/items");
     console.log(response.data);
+    setItems(response.data);
   };
 
   useEffect(() => {
     getItems();
   }, []);
 
-  return <div>Items</div>;
+  return (
+    <Container>
+      {items.map((item) => (
+        <ItemList
+          key={item.id}
+          name={item.itemName}
+          price={item.itemPrice}
+          quantity={item.stockQuantity}
+        />
+      ))}
+    </Container>
+  );
 };
 
 export default Items;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;

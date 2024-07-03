3bb485a04af70b8d16345e93567d88e2899d27cf
caminobelllo <alstpgml0812@gmail.com>
2024-07-03 17:31:18 +0900

    design: items table 스타일링

--- src/pages/Items.jsx
+++ src/pages/Items.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { axiosInstance } from "../api";
-import ItemList from "../components/items/ItemList";
 import styled from "styled-components";
+
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 
 const Items = () => {
   const [items, setItems] = useState([]);
@@ -18,14 +20,74 @@ const Items = () => {
 
   return (
     <Container>
-      {items.map((item) => (
+      {/* {items.map((item) => (
         <ItemList
           key={item.id}
           name={item.itemName}
           price={item.itemPrice}
           quantity={item.stockQuantity}
         />
-      ))}
+      ))} */}
+      <DataTable
+        value={items}
+        responsibleLayout="scroll"
+        dataKey="id"
+        paginator
+        rows={5}
+        showGridlines
+        stripedRows
+        emptyMessage="-"
+        tableStyle={{
+          width: "65rem",
+          height: "20rem",
+          textAlign: "center",
+        }}
+      >
+        <Column
+          field="id"
+          header="ID"
+          style={{ width: "25%" }}
+          headerStyle={{
+            fontSize: "24px",
+            fontWeight: "800",
+            padding: "2rem 0 2rem 6.5rem",
+            backgroundColor: "#f3f3f3",
+          }}
+        ></Column>
+        <Column
+          field="itemName"
+          header="상품명"
+          style={{ width: "25%" }}
+          headerStyle={{
+            fontSize: "24px",
+            fontWeight: "800",
+            padding: "2rem 0 2rem 6.5rem",
+            backgroundColor: "#f3f3f3",
+          }}
+        ></Column>
+        <Column
+          field="itemPrice"
+          header="가격"
+          style={{ width: "25%" }}
+          headerStyle={{
+            fontSize: "24px",
+            fontWeight: "800",
+            padding: "2rem 0 2rem 6.5rem",
+            backgroundColor: "#f3f3f3",
+          }}
+        ></Column>
+        <Column
+          field="stockQuantity"
+          header="수량"
+          style={{ width: "25%" }}
+          headerStyle={{
+            fontSize: "24px",
+            fontWeight: "800",
+            padding: "2rem 0 2rem 6.5rem",
+            backgroundColor: "#f3f3f3",
+          }}
+        ></Column>
+      </DataTable>
     </Container>
   );
 };
@@ -33,6 +95,9 @@ const Items = () => {
 export default Items;
 
 const Container = styled.div`
+  margin-top: 100px;
+  font-size: 20px;
+  width: 100%;
   display: flex;
-  flex-direction: column;
+  justify-content: center;
 `;
